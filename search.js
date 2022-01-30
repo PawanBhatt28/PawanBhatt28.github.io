0b3b7d7046b099a25e8fa898cc21860da30a2f79
PawanBhatt <imcool.pawanbhattjarvis@gmail.com>
2022-01-30 16:58:20 +0530

    profiles searchbar working

--- search.js
+++ search.js
@@ -5,16 +5,17 @@ function searchResult(){
     const search = document.querySelector('#search').value.toLowerCase();
     const allboxes = document.querySelector('.main-body').children;
 
-    console.log(search);
+    console.log(`Search input : ${search}`);
 
 
     for(let box of allboxes){
         const title = box.querySelector('#name p').textContent.toLowerCase();
-        
+        console.log(`Title : ${title}`);
         if(title.includes(search)){
             box.classList.remove('hideIt');
         }else{
             box.classList.add('hideIt');
         }
     }
+    
 }
